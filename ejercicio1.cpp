b50efdc8c32e19913d307fb76e67b76614df2fcd
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-16 15:24:36 -0300

    Agregar ejercicio1.cpp

---
+++ ejercicio1.cpp
@@ -0,0 +1,111 @@
+/* 1.-
+
+Teniendo estas definiciones de objetos:
+
+class Dispositivo{
+
+	string dir_ip;
+	string nombre;
+	string mascara;
+
+public:
+	Dispositivo(){}
+
+	void enviar_datos(Paquete, Dispositivo);
+	void recibir_datos(Paquete, Dispositivo);
+
+}
+
+class Paquete{
+
+	Dispositivo origen;
+	Dispositivo destino;
+	string mensaje;
+
+public:
+
+	Paquete(Dispositivo, Dispositivo, string);
+	void mostrar();
+
+
+}
+
+a) Modificar el constructor de la clase Dispositivo para que pueda recibir los datos necesarios e
+inicializar los atributos.
+b) Implementar los m�todos enviar_datos y recibir_datos para que ambos muestren por pantalla los
+parametros que recibieron.
+c) Implementar el constructor de la clase Paquete
+d) Implementar el m�todo mostrar();
+e) En la funci�n main, declarar los objetos y probarlos.
+
+OPCIONAL IMPORTANTE
+
+En la funci�n main, declarar:
+
+vector<Dispositivo> red;
+
+- utilizarlo como contenedor de dispositivos, y luego, recorrerlo, mostrando los dispositivos que
+componen a esa red.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Dispositivo{
+
+	string dir_ip;
+	string nombre;
+	string mascara;
+
+public:
+	Dispositivo(dir_ip, nombre, mascara){
+        dir_ip = " ";
+        nombre = " ";
+        mascara = " ";
+        cout << "Dispositivo inicializado" << endl;
+    }
+	void enviar_datos(Paquete, Dispositivo);
+	void recibir_datos(Paquete, Dispositivo);
+
+};
+
+void enviar_datos(Paquete, Dispositivo) {
+    cout << Dispositivo << " " << Paquete << endl;
+}
+
+void recibir_datos(Paquete, Dispositivo){
+    cout << Paquete << " " << Dispositivo << endl;
+}
+
+class Paquete{
+
+	Dispositivo origen;
+	Dispositivo destino;
+	string mensaje;
+
+public:
+
+	Paquete(Dispositivo, Dispositivo, string mensaje) {
+        // Dispositivo
+        // Dispositivo
+        mensaje = " ";
+    }
+	void mostrar();
+
+
+}
+
+using namespace std;
+
+int main () {
+
+Dispositivo A;
+
+vector <Dispositivo> red;
+
+for (int i = 0; i < Dispositivo.size(); i++) {
+    cout << Dispositivo[i] << endl;
+}
+
+return 0;
+}
