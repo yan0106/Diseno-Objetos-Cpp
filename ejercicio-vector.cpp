3682fc941bc3b075b506426f4f9ccc87deb40d01
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-28 21:19:20 -0300

    Agregar ejercicio-vector.cpp

---
+++ ejercicio-vector.cpp
@@ -0,0 +1,28 @@
+/* Ejercicio: lista de Vectores de cuatro elementos 
+
+Estructura básica: 
+
+class vec4{ 
+
+private: 
+    float v1, v2, v3, v4; 
+
+public: 	
+    Dos constructores, uno inicializando en 0.0 los componentes otro que recibe cuatro floats
+
+    metodos gets y sets para cada uno de los componentes 
+
+    una funcion mostrar() que imprime por pantalla el vector 
+};
+
+class nodo{ 
+    vec4 vect; 
+	nodo* sig;
+
+public: 
+    nodo(){sig=NULL;} 
+	void set_nodo(vec4); 
+	void set_sig(nodo a); bool 	si_esta(vec4); 
+	void mostrar(); 
+};
+*/
