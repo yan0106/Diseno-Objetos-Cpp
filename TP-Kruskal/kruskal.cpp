cdeafa686720525a015aea336af66f9efdc46167
Yandira Diaz <yandiradiaz@hotmail.com>
2026-05-10 19:35:26 -0300

    implementar método combina

--- TP-Kruskal/kruskal.cpp
+++ TP-Kruskal/kruskal.cpp
@@ -40,3 +40,36 @@ return ' '; // si sale del bucle for y no lo encontró, retorna vacío
 }
 // clave: nombre del grupo | valor: par de (cantidad, 1er. elem.)
 // map <char, pair<int, char>> encabezados;
+
+void grafo::combina(const char& grupoA, const char& grupoB){ // combina las aristas que se encuentran formando el árbol    
+    
+    // variables para conocer qué grupo es más grande y unirlos (al grupo más grande le suma el más chico)
+    char g_grande;
+    char g_chico;    
+    if (CE.encabezados[grupoA].first > CE.encabezados[grupoB].first) { // comparo las cant. de v de los grupos (los int)
+        g_grande = grupoA;
+        g_chico = grupoB;
+    } else {
+        g_grande = grupoB;
+        g_chico = grupoA;
+    }
+     // recorre los vértices:
+    char vertice = CE.encabezados[g_grande].second; // guarda el vértice actual desde donde empieza  
+    while (CE.nombres[vertice].second != ' ') { // camina por los vértices, mientras que no llegue a ' ';
+        vertice = CE.nombres[vertice].second; // actualiza el vertice actual
+    }
+
+    // actualiza el último v de g_grande:
+    char primer_v_chico = CE.encabezados[g_chico].second; // guarda en una variable, el 1er. v del g_chico
+    CE.nombres[vertice].second = primer_v_chico; // al último v de g_grande, le asigna el 1er. v del g_chico
+
+    // actualiza el v anterior:
+    CE.nombres[primer_v_chico].first = vertice;
+
+    // atualiza la cantidad del grupo:
+    CE.encabezados[g_grande].first += CE.encabezados[g_chico].first;
+
+    // anula el g_chico:
+    CE.encabezados[g_chico].first = 0; // cantidad 0
+    CE.encabezados[g_chico].second = ' '; // primer v como ' '
+} 
